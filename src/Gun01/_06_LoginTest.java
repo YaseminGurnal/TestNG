821fa6abefe656a3821d490c4888a45c05da4d57
YaseminGurnal <ysmngurnal@gmail.com>
2025-09-05 13:33:32 +0300

    Günlük pratik-05 Eylül

---
+++ src/Gun01/_06_LoginTest.java
@@ -0,0 +1,41 @@
+package Gun01;
+
+import Utility.BaseDriver;
+import Utility.Tools;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class _06_LoginTest extends BaseDriver {
+    //  Senaryo
+//  1- Siteye Login olunuz
+//  2- Login olduğunuzu Title ile Assert yapınız.
+
+    @Test
+    public void Test1(){
+
+        driver.get("https://opencart.abstracta.us/index.php?route=account/login");
+        Tools.Bekle(3);
+
+        WebElement email= driver.findElement(By.id("input-email"));
+        email.sendKeys("gizem@gmail.com");
+        Tools.Bekle(2);
+
+        WebElement password= driver.findElement(By.id("input-password"));
+        password.sendKeys("G1991");
+        Tools.Bekle(2);
+
+        WebElement loginButton= driver.findElement(By.xpath("//input[@type='submit']"));
+        loginButton.click();
+        Tools.Bekle(2);
+
+        wait.until(ExpectedConditions.titleIs("My Account"));
+        Assert.assertTrue(driver.getTitle().equals("My Account"),"Login olunamadı.");
+        Tools.Bekle(2);
+
+          driver.quit();
+    }
+
+}
