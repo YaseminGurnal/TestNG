61bb7d517203032762259fab2b5f94af2415a2a3
YaseminGurnal <ysmngurnal@gmail.com>
2025-09-12 19:43:21 +0300

    12 Eylül

---
+++ src/Gun04/_03_SearchFuncionality.java
@@ -0,0 +1,33 @@
+package Gun04;
+
+import Utility.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class _03_SearchFuncionality extends BaseDriver {
+    /*
+      Senaryo ;
+      1- Siteyi açınız.
+      2- mac kelimeini göndererek aratınız.
+      3- Çıkan sonuçlarda mac kelimesinin geçtiğini doğrulayınız.
+      4- aynı işlemi samsung için de yapınız
+    */
+    @Test
+    @Parameters("Search Text")
+    public void SearchFunction(String arananKelime) {
+
+        WebElement searchInput = driver.findElement(By.xpath("//input[@class='form-control input-lg']"));
+        searchInput.sendKeys(arananKelime + Keys.ENTER);
+
+      List<WebElement> captions=driver.findElements(By.cssSelector("[class='caption']>h4"));
+      for (WebElement e : captions)
+          Assert.assertTrue(e.getText().toLowerCase().contains(arananKelime.toLowerCase()),"Aranan kelime bulunamadı.");
+
+    }
+}
